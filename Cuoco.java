248f1524865ff600c5ec3141956d1ee6e78d825a
fiippogigli <76041375+fiippogigli@users.noreply.github.com>
2021-05-12 13:42:19 +0200

    Add files via upload

---
+++ Cuoco.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author informatica
+ */
+public class Cuoco extends Thread{
+
+    int piatto = 0;
+    Bancone bancone1;
+    static int numeroPiatti = 0;
+    
+    
+    //COSTRUTTORE
+    public Cuoco() {
+        
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
